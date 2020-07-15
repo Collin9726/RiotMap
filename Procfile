7abfc87468a8afa1ea1cc4ee9f276feb73092257
collin9726 <owino.collin@gmail.com>
2020-07-15 14:31:46 +0300

    create project and add boilerplate

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mapsproject.wsgi --log-file -
---
+++ maps/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+
+
+# Create your views here.
+def welcome(request):
+    return render(request, 'index.html')
