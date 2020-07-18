8e37c501df87b38b968ce765503cbdc9fe062d79
stephen remmi <stephenremmi@gmail.com>
2020-07-18 12:33:40 +0300

    Add Riots model to models.py

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+from django.db.models import Manager as GeoManager
 # Create your models here.
+class Riots(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=250, null=True)
+    date_reported = models.DateField(auto_now_add=True)
+    location = models.PointField(srid=4326)
+    objects = GeoManager()
+
+    def __str__(self):
+		return self.title
