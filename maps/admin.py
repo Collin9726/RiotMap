a9309b798193fec0e6849c2a78047fa399a6cd3d
stephen remmi <stephenremmi@gmail.com>
2020-07-18 14:20:03 +0300

    Change Riots to Riot due to pluralization and register County model in the admin.py for admin interface display

--- maps/admin.py
+++ maps/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Riots
+from .models import Riot, County
 from leaflet.admin import LeafletGeoAdmin
 # Register your models here.
-@admin.register(Riots)
-class RiotsAdmin(LeafletGeoAdmin):
+@admin.register(Riot)
+class RiotAdmin(LeafletGeoAdmin):
     list_display = ('title', 'location')
+
+class CountyAdmin(LeafletGeoAdmin):
+    list_display = ('counties', 'codes')
+
+admin.site.register(Riots, RiotsAdmin)
+admin.site.register(County, CountyAdmin)
--- maps/models.py
+++ maps/models.py
@@ -3,7 +3,7 @@ from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
 from django.db.models import Manager as GeoManager
 # Create your models here.
-class Riots(models.Model):
+class Riot(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=250, null=True)
     date_reported = models.DateField(auto_now_add=True)
@@ -12,7 +12,7 @@ class Riots(models.Model):
 
     def __str__(self):
         return self.title
-        
+
 class County(models.Model):
     counties = models.CharField(max_length=25)
     codes = models.IntegerField()
