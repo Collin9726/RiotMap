d2fe44bdc42b6b36be29b1dcb3e57659b9d3616e
stephen remmi <stephenremmi@gmail.com>
2020-07-18 12:53:43 +0300

    Tweak code in admin module to register Riots model for admin interface display

--- maps/admin.py
+++ maps/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.gis.admin import OSMGeoAdmin
+from models import Riots
 # Register your models here.
+@admin.register(Riots)
+class RiotsAdmin(OSMGeoAdmin):
+    list_display = ('title', 'location')
