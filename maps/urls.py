0c7f1e13d6f78f6ad676176dd9a8cf5772409dc7
stephen remmi <stephenremmi@gmail.com>
2020-07-19 17:34:00 +0300

    Add views and urls

--- maps/urls.py
+++ maps/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),    
+    url(r'^$',views.welcome,name = 'welcome'),
+    url('riot/', views.RiotData , name='riots'),
+    url('county/', views.CountyData, name='counties'),    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
