473880efc8ff751f631c5df2bc470c702ed74a3d
stephen remmi <stephenremmi@gmail.com>
2020-07-19 23:19:15 +0300

    Import User

--- maps/models.py
+++ maps/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
 from django.db.models import Manager as GeoManager
+from django.contrib.auth.models import User
 # Create your models here.
 class Riot(models.Model):
     title = models.CharField(max_length=20)
