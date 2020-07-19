58e6ccaa573694acd5081009efb2a8364c50ccea
stephen remmi <stephenremmi@gmail.com>
2020-07-19 23:34:58 +0300

    Add profile views

--- maps/views.py
+++ maps/views.py
@@ -16,6 +16,31 @@ def CountyData(request):
     counties = serialize('geojson', County.objects.all())
     return HttpResponse(counties,content_type='json')
 
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = CountyForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.userId = request.user.id
+            profile.save()
+        return redirect('NewProfile')
+    else:
+        form = CountyForm()
+    return render(request, 'new_profile.html', {"form": form})
+
+def profile(request):
+    current_user = request.user
+    riots = Riot.objects.filter(profile = current_user)
+
+    try:
+        profile = Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile.html',{ 'profile':profile,'riots':riots,'current_user':current_user})
+
 
 
 
